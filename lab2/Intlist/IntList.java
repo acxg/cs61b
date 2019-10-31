4af8343d842a81da664355aa3a77f60a6ae6c896
Betty S <jiaxuan@24-124-72-41-dynamic.midco.net>
2019-10-30 21:49:45 -0500

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList res = A;
+        while(res.rest!=null){
+            res = res.rest;
+        }
+        res.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +96,19 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A==null){
+            return B;
+        }
+        IntList res = new IntList(A.first,null);
+        IntList temp = res;
+        A = A.rest;
+        while(A!= null ){
+            temp.rest = new IntList(A.first,null);
+            A = A.rest;
+            temp = temp.rest;
+        }
+        temp.rest = B;
+        return res;
     }
 
 
